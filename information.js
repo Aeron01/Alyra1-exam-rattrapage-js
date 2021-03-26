f2cb2076c06cfed5687d24c2dbf915a678ea280c
Aeron01 <one.soulreaper@gmail.com>
2021-03-26 17:01:36 +0100

    information done

--- information.js
+++ information.js
@@ -1,27 +1,27 @@
 
-const information = (maj, lastName, firstName, age, sex) => {
-  age = Number(age)
+const information = (lastName, firstName, age, sex) => {
+
   console.log(`prenom: ${lastName}`)
   console.log(`nom: ${firstName}`)
   console.log(`age: ${age}`)
   switch (sex) {
     case ('M'):
       if (age < maj) {
-        return console.log(`Monsieur, vous serez majeur dans ${age - maj} ans`)
+        return console.log(`Monsieur, vous serez majeur dans ${maj - age} ans`)
       } else {
         return console.log(`Monsieur, vous êtes majeur depuis ${age - maj} ans`)
       }
       break
     case ('F'):
       if (age < maj) {
-        return console.log(`Madame, vous serez majeur dans ${age - maj} ans`)
+        return console.log(`Madame, vous serez majeur dans ${maj - age} ans`)
       } else {
         return console.log(`Madame, vous êtes majeur depuis ${age - maj} ans`)
       }
       break
     case ('?'):
       if (age < maj) {
-        return console.log(`Vous serez majeur dans ${age - maj} ans`)
+        return console.log(`Vous serez majeur dans ${maj - age} ans`)
       } else {
         return console.log(`Vous êtes majeur depuis ${age - maj} ans`)
       }
